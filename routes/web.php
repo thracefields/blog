e39acd872ef9f5a454b0398edc4e0a2de6af57c4
Iliyan Todorov <thracefields@localhost.localdomain>
2020-09-26 20:40:38 +0300

    Added admin page

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/contacts', 'PagesController@contacts')->name('contacts');
+
+Route::view('/admins', 'admins');
